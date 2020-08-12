f42905613555583eeee6da0800377a8792e9f7a7
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-12 11:00:16 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,11 +1,11 @@
-class ChattyStudent<Student
+class ChattyStudent < Student
   def hello
     super
     puts "How are you doing today? I'm okay, mbut kind of tired. Did you watch The Walking Dead last night? You didn't? Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me tell you who died..."
   end 
   def raise_hand
     super
-    10.times do 
+    ten.times do 
     puts "Pick me!"
   end 
 end
