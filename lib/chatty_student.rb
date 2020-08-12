5fab525884b06d6b49ed40a70822e36553e969ce
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-12 11:01:22 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,11 +1,11 @@
 class ChattyStudent < Student
   def hello
     super
-    puts "How are you doing today? I'm okay, mbut kind of tired. Did you watch The Walking Dead last night? You didn't? Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me tell you who died..."
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me tell you who died..."
   end 
   def raise_hand
     super
-    ten.times do 
+    10.times do 
     puts "Pick me!"
   end 
 end
